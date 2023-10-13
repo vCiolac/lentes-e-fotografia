afa9f08951b7a4b1bf94196c6c1a26352bb15b2f
Victor Ciolac <victor.bcfraga@gmail.com>
2023-10-13 00:00:06 -0300

    fixing footer

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -6,9 +6,9 @@ import { setAlbumCover } from '../../hooks/changeAlbumCover';
 import { Context } from '../../context/Context';
 import Notification from '../../components/Notification/Notification';
 import { formatAlbumName } from '../../hooks/handleAlbumName';
-import Footer from '../../components/Footer/Footer';
 import { Album } from '../../types';
 import LoadingSpinner from '../../components/Loading/Loading';
+import GoogleIcon from '../../assets/google.svg';
 
 function Events() {
   const { albumname } = useParams();
@@ -34,8 +34,6 @@ function Events() {
       setSelectedImages([...selectedImages, album]);
     }
   };
-
-  console.log(albumCovers)
 
   const handleOpenCoverSelection = () => {
     setIsCoverSelectionOpen(true);
@@ -79,10 +77,10 @@ function Events() {
   }
   const headerImg = eventCover();
 
-    return (
-      <main className='w-full'>
-        <header className="flex-col self-stretch relative flex w-full max-md:max-w-full">
-          <Link to="/" >
+  return (
+    <main className='w-full'>
+      <header className="flex-col self-stretch relative flex w-full max-md:max-w-full">
+        <Link to="/" >
           <img
             loading="lazy"
             srcSet={headerImg}
@@ -92,100 +90,104 @@ function Events() {
           <div className="relative text-white text-3xl font-semibold uppercase self-stretch items-center w-full mt-32 pt-6 pb-2 px-5 max-md:max-w-full max-md:pl-2.5">
             {tittle}
           </div>
-          </Link>
-        </header>
-        {user && user.uid === import.meta.env.VITE_REACT_APP_ADMIN_UID && (
-          <div className='flex flex-col'>
-            <span className="text-black text-base font-semibold mt-1 max-md:max-w-full">
-              Bem-vindo administrador.
-            </span>
-            <button onClick={handleOpenCoverSelection}>Trocar Capa do Álbum</button>
-            {isCoverSelectionOpen && (
-              <div>
-                <select value={selectedCover} onChange={handleCoverSelection}>
-                  <option value="">Escolha uma capa</option>
-                  {albums.map((album) => (
-                    <option key={album.nome} value={album.url}>
-                      {album.nome}
-                    </option>
-                  ))}
-                </select>
-                <button onClick={handleSaveCover}>Salvar Capa</button>
-              </div>
-            )}
-          </div>
-        )}
-        <div className='mt-6 ml-6'>
-          <span className='text-black text-base font-semibold'>
-            Descrição sobre como achar o horário de quando as fotos foram tiradas.
+        </Link>
+      </header>
+      {user && user.uid === import.meta.env.VITE_REACT_APP_ADMIN_UID && (
+        <div className='flex flex-col'>
+          <span className="text-black text-base font-semibold mt-1 max-md:max-w-full">
+            Bem-vindo administrador.
           </span>
-          <div className="flex flex-row flex-wrap content-center max-md:justify-center align-middle my-6 w-full">
+          <button onClick={handleOpenCoverSelection}>Trocar Capa do Álbum</button>
+          {isCoverSelectionOpen && (
             <div>
-              <label htmlFor="filterDate" className="text-[#535353] font-open-sans mr-2">
-                Dia:
-              </label>
-              <select
-                id="filterDate"
-                className="md:pl-1 md:pr-5 border border-gray-500 rounded py-1 text-gray-700 focus:outline-none md:mr-6"
-                value={filterDate}
-                onChange={(e) => setFilterDate(e.target.value)}
-              >
-                <option value="">Selecione o dia</option>
-                {uniqueDates.map((date) => (
-                  <option key={date} value={date}>
-                    {date}
+              <select value={selectedCover} onChange={handleCoverSelection}>
+                <option value="">Escolha uma capa</option>
+                {albums.map((album) => (
+                  <option key={album.nome} value={album.url}>
+                    {album.nome}
                   </option>
                 ))}
               </select>
+              <button onClick={handleSaveCover}>Salvar Capa</button>
             </div>
-            <div>
-              <label htmlFor="filterTimeRange" className="text-[#535353] font-open-sans mr-2">
-                Horário:
-              </label>
-              <select
-                id="filterTimeRange"
-                value={filterTimeRange}
-                onChange={(e) => setFilterTimeRange(e.target.value)}
-                className="pl-1 border border-gray-500 rounded py-1 text-gray-700 focus:outline-none mt-2 md:mt-0"
-              >
-                <option value="">Filtrar por hora</option>
-                {allTimeRanges.map((timeRange) => (
-                  <option key={timeRange} value={timeRange}>
-                    {timeRange}
-                  </option>
-                ))}
-              </select>
-            </div>
+          )}
+        </div>
+      )}
+      <div className='mt-6 ml-6'>
+        <span className='text-black text-base font-semibold'>
+          Descrição sobre como achar o horário de quando as fotos foram tiradas.
+        </span>
+        <div className="flex flex-row flex-wrap content-center max-md:justify-center align-middle my-6 w-full">
+          <div>
+            <label htmlFor="filterDate" className="text-[#535353] font-open-sans mr-2">
+              Dia:
+            </label>
+            <select
+              id="filterDate"
+              className="md:pl-1 md:pr-5 border border-gray-500 rounded py-1 text-gray-700 focus:outline-none md:mr-6"
+              value={filterDate}
+              onChange={(e) => setFilterDate(e.target.value)}
+            >
+              <option value="">Selecione o dia</option>
+              {uniqueDates.map((date) => (
+                <option key={date} value={date}>
+                  {date}
+                </option>
+              ))}
+            </select>
           </div>
-          <div className="flex flex-col md:relative md:right-36 md:flex-row md:space-x-6 md:ml-1 mt-3 md:mt-0 justify-between md:justify-start flex-wrap pt-2 md:pt-0 pr-4">
-            {/* <button className="bg-[#3FB8DE] hover:bg-[#45889e] rounded-lg px-3 py-1 shadow font-opens-sans" onClick={applyFilter}>Aplicar Filtro</button> */}
-            {/* <button className="bg-red-400 hover:bg-red-500 rounded-lg px-3 py-1 shadow font-opens-sans" onClick={clearFilters}>Limpar Filtros</button> */}
+          <div>
+            <label htmlFor="filterTimeRange" className="text-[#535353] font-open-sans mr-2">
+              Horário:
+            </label>
+            <select
+              id="filterTimeRange"
+              value={filterTimeRange}
+              onChange={(e) => setFilterTimeRange(e.target.value)}
+              className="pl-1 border border-gray-500 rounded py-1 text-gray-700 focus:outline-none mt-2 md:mt-0"
+            >
+              <option value="">Filtrar por hora</option>
+              {allTimeRanges.map((timeRange) => (
+                <option key={timeRange} value={timeRange}>
+                  {timeRange}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
-        {filteredAlbums.length === 0 && <LoadingSpinner msg='Carregando imagens...' />}
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mb-10 justify-items-center mx-10">
-          {filteredAlbums.map((album) => (
-            <li key={album.nome} className="bg-white p-2 rounded shadow w-full relative group">
-              <div onClick={() => handleImageClick(album)} className="relative">
-                <div className="w-full h-80 relative">
-                  <img
-                    src={album.url}
-                    alt={album.nome}
-                    className={`w-full h-full object-cover rounded shadow group-hover:opacity-80 transition duration-500 ease-in-out ${selectedImages.includes(album) ? 'border-[0.5rem] border-solid border-[#3Fb8dE]' : ''}`}
-                  />
-                  <div className="absolute inset-0 bg-gradient-to-t rounded from-gray-900 via-transparent to-transparent" />
-                </div>
-                <span className="absolute bottom-0 left-0 right-0 p-2 font-bold text-center text-white group-active:text-red-300 font-open-sans">
-                  {album.exifData.data}
-                </span>
+        <div className="flex flex-col md:relative md:right-36 md:flex-row md:space-x-6 md:ml-1 mt-3 md:mt-0 justify-between md:justify-start flex-wrap pt-2 md:pt-0 pr-4">
+          {/* <button className="bg-[#3FB8DE] hover:bg-[#45889e] rounded-lg px-3 py-1 shadow font-opens-sans" onClick={applyFilter}>Aplicar Filtro</button> */}
+          {/* <button className="bg-red-400 hover:bg-red-500 rounded-lg px-3 py-1 shadow font-opens-sans" onClick={clearFilters}>Limpar Filtros</button> */}
+        </div>
+      </div>
+      {filteredAlbums.length === 0 && <LoadingSpinner msg='Carregando imagens...' />}
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mb-10 justify-items-center mx-10">
+        {filteredAlbums.map((album) => (
+          <li key={album.nome} className="bg-white p-2 rounded shadow w-full relative group">
+            <div onClick={() => handleImageClick(album)} className="relative">
+              <div className="w-full h-80 relative">
+                <img
+                  src={album.url}
+                  alt={album.nome}
+                  className={`w-full h-full object-cover rounded shadow group-hover:opacity-80 transition duration-500 ease-in-out ${selectedImages.includes(album) ? 'border-[0.5rem] border-solid border-[#3Fb8dE]' : ''}`}
+                />
+                <div className="absolute inset-0 bg-gradient-to-t rounded from-gray-900 via-transparent to-transparent" />
               </div>
-            </li>
-          ))}
-        </ul>
-        {message && <Notification message={message} />}
-        <Footer />
-      </main >
-    );
-  }
+              <span className="absolute bottom-0 left-0 right-0 p-2 font-bold text-center text-white group-active:text-red-300 font-open-sans">
+                {album.exifData.data}
+              </span>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {message && <Notification message={message} />}
+      <footer className={`bg-[#3091A8] h-10 w-full ${filteredAlbums.length === 0 ? 'absolute' : 'relative'} bottom-0 flex items-center justify-end`}>
+        <Link to="/login" className="">
+          <img src={GoogleIcon} alt="Google Icon" width="20" className='mr-2' />
+        </Link>
+      </footer>
+    </main >
+  );
+}
 
-  export default Events;
+export default Events;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,21 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import FetchAlbums from '../../hooks/fetchAlbums';
 import Header from '../../components/Header/Header';
 import { formatAlbumName } from '../../hooks/handleAlbumName';
 import LoadingSpinner from '../../components/Loading/Loading';
-import Footer from '../../components/Footer/Footer';
 import { Context } from '../../context/Context';
+import { Link } from 'react-router-dom';
+import GoogleIcon from '../../assets/google.svg';
 
 function Home() {
   const { albums, loading } = FetchAlbums();
   const { albumCovers } = useContext(Context);
 
   return (
-    <div className='min-h-screen relative'>
-      <Header />
+    <div>
+      <div className='h-full relative'>
+        <Header />
+      </div>
       <div className="container mx-auto">
         {loading ? (
           <LoadingSpinner msg='Carregando...' />
@@ -21,30 +23,35 @@ function Home() {
           <div>
             <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-10 justify-items-center mx-10">
               {albums.map((albumName) => (
-                <li key={albumName} className="bg-white p-2 rounded shadow w-[22rem] md:w-96 relative group">
+                <li key={albumName} className="bg-white p-2 rounded shadow w-full relative group">
+                  {/* <li key={albumName} className="bg-white p-2 rounded shadow w-[22rem] md:w-96 relative group"> */}
                   <div className="relative">
-                    <div className="w-full h-60 relative">
+                    <div className="w-full h-[16.3rem] relative">
+                      {/* <div className="w-full h-60 relative"> */}
                       <img
                         src={albumCovers[albumName]}
                         alt={albumName}
-                        className="w-full h-full object-cover rounded shadow group-hover:opacity-50 transition duration-500 ease-in-out"
+                        className={`w-full h-full object-cover rounded shadow group-hover:opacity-80 transition duration-500 ease-in-out`}
                       />
                       <div className="absolute inset-0 bg-gradient-to-t rounded from-gray-900 via-transparent to-transparent" />
                     </div>
                     <Link to={`/eventos/${albumName}`}>
-                      <span className="absolute bottom-0 left-0 right-0 p-2 font-bold text-center text-white group-hover:text-gray-300 font-open-sans">
+                      <span className="absolute bottom-0 left-0 right-0 p-2 font-bold text-center text-white group-active:text-red-300 font-open-sans">
                         {formatAlbumName(albumName)}
                       </span>
                     </Link>
                   </div>
-
                 </li>
               ))}
             </ul>
-            <Footer />
           </div>
         )}
       </div>
+      <footer className={`bg-[#3091A8] h-10 w-full ${loading ? 'absolute' : 'relative'} bottom-0 flex items-center justify-end`}>
+        <Link to="/login" className="">
+          <img src={GoogleIcon} alt="Google Icon" width="20" className='mr-2' />
+        </Link>
+      </footer>
     </div>
   );
 }
