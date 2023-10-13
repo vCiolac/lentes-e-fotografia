59544ef31e1c0f1d99b913fbad0192d6518d2b8e
Victor Ciolac <victor.bcfraga@gmail.com>
2023-10-12 22:52:53 -0300

    footer com login

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -20,13 +20,3 @@ const database = getFirestore(app);
 const storage = getStorage(app);
 
 export { auth, database, storage };
-
-// const firebaseConfig = {
-//   apiKey: process.env.VITE_REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.VITE_REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID,
-// };
