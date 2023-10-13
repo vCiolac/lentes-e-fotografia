b96db3ded02375cc540f97d680f6f4f8588c4f65
Victor Ciolac <victor.bcfraga@gmail.com>
2023-10-12 21:37:45 -0300

    fix footer position

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -16,33 +16,35 @@ function Home() {
       <Header />
       <div className="container mx-auto pb-8">
         {loading ? (
-          <LoadingSpinner msg='Carregando...'/>
+          <LoadingSpinner msg='Carregando...' />
         ) : (
-          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-10 justify-items-center mx-10">
-            {albums.map((albumName) => (
-              <li key={albumName} className="bg-white p-2 rounded shadow w-[22rem] md:w-96 relative group">
-                <div className="relative">
-                  <div className="w-full h-60 relative">
-                    <img
-                      src={albumCovers[albumName]}
-                      alt={albumName}
-                      className="w-full h-full object-cover rounded shadow group-hover:opacity-50 transition duration-500 ease-in-out"
-                    />
-                    <div className="absolute inset-0 bg-gradient-to-t rounded from-gray-900 via-transparent to-transparent" />
+          <div>
+            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-10 justify-items-center mx-10">
+              {albums.map((albumName) => (
+                <li key={albumName} className="bg-white p-2 rounded shadow w-[22rem] md:w-96 relative group">
+                  <div className="relative">
+                    <div className="w-full h-60 relative">
+                      <img
+                        src={albumCovers[albumName]}
+                        alt={albumName}
+                        className="w-full h-full object-cover rounded shadow group-hover:opacity-50 transition duration-500 ease-in-out"
+                      />
+                      <div className="absolute inset-0 bg-gradient-to-t rounded from-gray-900 via-transparent to-transparent" />
+                    </div>
+                    <Link to={`/eventos/${albumName}`}>
+                      <span className="absolute bottom-0 left-0 right-0 p-2 font-bold text-center text-white group-hover:text-gray-300 font-open-sans">
+                        {formatAlbumName(albumName)}
+                      </span>
+                    </Link>
                   </div>
-                  <Link to={`/eventos/${albumName}`}>
-                    <span className="absolute bottom-0 left-0 right-0 p-2 font-bold text-center text-white group-hover:text-gray-300 font-open-sans">
-                      {formatAlbumName(albumName)}
-                    </span>
-                  </Link>
-                </div>
 
-              </li>
-            ))}
-          </ul>
+                </li>
+              ))}
+            </ul>
+            <Footer />
+          </div>
         )}
       </div>
-      <Footer/>
     </div>
   );
 }
